ccb88771b4405e3ea89acea18169f38dbf02f427
Fidahasm <fidahkt@gmail.com>
2024-07-29 22:40:06 +0530

    js initiated

---
+++ script.js
@@ -0,0 +1,11 @@
+const start_container = document.getElementById("start-container");
+const button4 = document.getElementById("4x4");
+const button6 = document.getElementById("6x6");
+const start_button = document.getElementById("start-btn");
+const tile_container = document.getElementById("tile-container");
+
+function ChooseGrid()
+{
+
+}
+
