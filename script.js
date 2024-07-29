d609483a4f6c6ee1239e7034fd52384e9a6a85a3
Fidahasm <fidahkt@gmail.com>
2024-07-30 01:00:59 +0530

    js updated

--- script.js
+++ script.js
@@ -15,14 +15,50 @@ function ChooseGrid(value)
 function StartGame(){
     const main_container = document.getElementById("main-container")
     const start_container = document.getElementById("start-container");
+    const tile_container = document.getElementById("tile-container");
+
 
     if(grid_count==null){
         alert("Please choose a grid size");
     }
     else{
         main_container.style.display="block";
-        start_container.style.display="none"
+        start_container.style.display="none";
+        
+        tile_container.style.gridTemplateColumns = `repeat(${grid_count}, 100px)`;
+        // tile_container.style.gridTemplateRows = gridTemplateColumnsValue;        
+        for(let i=0;i<grid_count*grid_count;i++)
+        {
+            const tile = document.createElement('div');
+            tile.classList.add('tile');
+            tile_container.appendChild(tile)
+
+        }
+        
     }
 }
+const pastelColors = [
+    "#FF6961",
+    "#AEC6CF",
+    "#77DD77",
+    "#FDFD96",
+    "#FFB347",
+    "#CBAACB",
+    "#FFD1DC",
+    "#CFCFC4",
+    "#A9A9A9",
+    "#F8F8FF",
+    "#D3D3D3",
+    "#A7E9AF",
+    "#FF9AA2",
+    "#B2FBA5",
+    "#C4AEAD",
+    "#778899",
+    "#C4DFAA",
+    "#99C5C4"
+  ];
+  const coloPalette = [...pastelColors,...pastelColors];
+
+
 
 
