cda24dd6b24f5b2b74dac51c135f63dadda429f4
Fidahasm <fidahkt@gmail.com>
2024-07-31 00:11:30 +0530

    js updated

--- script.js
+++ script.js
@@ -5,6 +5,44 @@ const start_button = document.getElementById("start-btn");
 const tile_container = document.getElementById("tile-container");
 const main_container = document.getElementById("main-container")
 grid_count = null;
+const pastelColors6 = [
+    "#FF5733",
+  "#33FF57",
+  "#3357FF",
+  "#FF33A1",
+  "#FFB533",
+  "#33FFF5",
+  "#A833FF",
+  "#F5FF33",
+  "#FF6F61",
+  "#6B5B95",
+  "#88B04B",
+  "#F7CAC9",
+  "#92A8D1",
+  "#955251",
+  "#B565A7",
+  "#009B77",
+  "#DD4124",
+  "#45B8AC"
+  ];
+  const colorPalette6 = [...pastelColors6,...pastelColors6];
+  const pastelColors4 = [
+    "#FF5733",
+    "#33FF57",
+    "#3357FF",
+    "#FF33A1",
+    "#FFB533",
+    "#33FFF5",
+    "#A833FF",
+    "#F5FF33"
+  ];
+  const colorPalette4 = [...pastelColors4,...pastelColors4];
+
+  let revealCount = 0;
+  let activeTile = null;
+  let awaitingEndOfMove = false;
+
+  
 function ChooseGrid(value)
 {
     grid_count = value;
@@ -21,43 +59,94 @@ function StartGame(){
     if(grid_count==null){
         alert("Please choose a grid size");
     }
-    else{
+    else if(grid_count==4){
         main_container.style.display="block";
         start_container.style.display="none";
         
         tile_container.style.gridTemplateColumns = `repeat(${grid_count}, 100px)`;
-        // tile_container.style.gridTemplateRows = gridTemplateColumnsValue;        
+
+        const count=  colorPalette4.length;
         for(let i=0;i<grid_count*grid_count;i++)
         {
-            const tile = document.createElement('div');
-            tile.classList.add('tile');
-            tile_container.appendChild(tile)
-
+            
+            const randomIndex = Math.floor(Math.random()*colorPalette4.length);
+            const color = colorPalette4[randomIndex];
+            const tile = buildTile(color,count);
+            colorPalette4.splice(randomIndex,1);
+            tile_container.appendChild(tile);
         }
+        
+
+    }
+    else{
+        main_container.style.display="block";
+        start_container.style.display="none";
+        tile_container.style.gridTemplateColumns = `repeat(${grid_count}, 100px)`;
+
+        const count = colorPalette6.length;
+        for(let i=0;i<grid_count*grid_count;i++)
+        {
+            const randomIndex = Math.floor(Math.random()*colorPalette6.length);
+            const color = colorPalette6[randomIndex];
+            const tile = buildTile(color,count);
+            colorPalette6.splice(randomIndex,1);
+            tile_container.appendChild(tile);
+        }
+        
         
     }
 }
-const pastelColors = [
-    "#FF6961",
-    "#AEC6CF",
-    "#77DD77",
-    "#FDFD96",
-    "#FFB347",
-    "#CBAACB",
-    "#FFD1DC",
-    "#CFCFC4",
-    "#A9A9A9",
-    "#F8F8FF",
-    "#D3D3D3",
-    "#A7E9AF",
-    "#FF9AA2",
-    "#B2FBA5",
-    "#C4AEAD",
-    "#778899",
-    "#C4DFAA",
-    "#99C5C4"
-  ];
-  const coloPalette = [...pastelColors,...pastelColors];
+var moves=0;
+
+function buildTile(color,count){
+    console.log(count)
+    const element = document.createElement('div');
+    element.classList.add('tile');
+    element.setAttribute('data-color',color);
+    element.setAttribute('revealed-data',"false");
+    // const revealedColor = element.getAttribute('data-color');
+   
+    
+    element.addEventListener('click',() =>{
+        
+        moves+=0.5;   
+        document.getElementById('moves').innerHTML = Math.floor(moves);
+        if (awaitingEndOfMove || element==activeTile||element.getAttribute('revealed-data') == "true"){
+            return;
+        }
+        element.style.backgroundColor = color;
+        if(!activeTile){
+            activeTile=element;
+            return;
+        }
+        const activeTileColor = activeTile.getAttribute('data-color')
+
+        if( activeTileColor === color){
+            activeTile.setAttribute('revealed-data',"true");
+            element.setAttribute('revealed-data',"true");
+            awaitingEndOfMove = false;
+            activeTile = null;
+            revealCount+=2;
+            if(revealCount===count){
+                alert("You won! Refresh and Play again.");
+            }
+            return;
+        }
+       
+        awaitingEndOfMove = true;
+        setTimeout(() =>{
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor =  null;
+            activeTile = null;
+            awaitingEndOfMove = false;
+        },1000);
+        
+
+    });
+    return element;
+   
+}
+
 
 
 
